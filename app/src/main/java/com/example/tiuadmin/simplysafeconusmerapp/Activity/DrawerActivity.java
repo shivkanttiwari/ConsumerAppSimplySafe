d4fd3d41373feefbc6923be85ccb2cfd6e90f3e0
TIU Admin <shivkant.tiwari@tiuconsulting.com>
2016-12-17 15:07:43 +0530

    Consumer App First Commit.

--- app/src/main/java/com/example/tiuadmin/simplysafeconusmerapp/Activity/DrawerActivity.java
+++ app/src/main/java/com/example/tiuadmin/simplysafeconusmerapp/Activity/DrawerActivity.java
@@ -1,16 +1,16 @@
 package com.example.tiuadmin.simplysafeconusmerapp.Activity;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
@@ -23,17 +23,15 @@ import com.example.tiuadmin.simplysafeconusmerapp.CustomAdapter.MerchantAdapter;
 import com.example.tiuadmin.simplysafeconusmerapp.Fragments.MainActivity;
 import com.example.tiuadmin.simplysafeconusmerapp.Models.Merchant;
 import com.example.tiuadmin.simplysafeconusmerapp.R;
+import com.example.tiuadmin.simplysafeconusmerapp.Rewards.BlankFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class DrawerActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
 
-    private RecyclerView recyclerView;
-    private MerchantAdapter adapter;
-    private List<Merchant> albumList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,18 +43,7 @@ public class DrawerActivity extends AppCompatActivity {
 
       //  initCollapsingToolbar();
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        albumList = new ArrayList<>();
-        adapter = new MerchantAdapter(this, albumList);
-
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
-
-        prepareAlbums();
 
 //        try {
 //            Glide.with(this).load(R.drawable.cover).into((ImageView) findViewById(R.id.backdrop));
@@ -74,14 +61,19 @@ public class DrawerActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(MenuItem menuItem) {
 
                 int id = menuItem.getItemId();
-
+                Fragment fragment=null;
+                FragmentManager fragmentManager = getSupportFragmentManager();
                 switch (id){
                     case R.id.home:
                         Toast.makeText(getApplicationContext(),"Home",Toast.LENGTH_SHORT).show();
                         drawerLayout.closeDrawers();
                         break;
-                    case R.id.settings:
+                    case R.id.contactus:
                         Toast.makeText(getApplicationContext(),"Settings",Toast.LENGTH_SHORT).show();
+                        fragment=new BlankFragment();
+                        getFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_place, fragment)
+                                .commit();
                         break;
                     case R.id.Rewards:
                         Toast.makeText(getApplicationContext(),"Trash",Toast.LENGTH_SHORT).show();
@@ -152,102 +144,6 @@ public class DrawerActivity extends AppCompatActivity {
 //        });
 //    }
 
-    /**
-     * Adding few albums for testing
-     */
-    private void prepareAlbums() {
-        int[] covers = new int[]{
-                R.drawable.flipkarthome,
-                R.drawable.amazonhome,
-                R.drawable.ebayhome,
-                R.drawable.snapdealhome,
-                R.drawable.bkshome,
-                R.drawable.olahome,
-                R.drawable.album7,
-                R.drawable.album8,
-                R.drawable.album9,
-                R.drawable.album10,
-                R.drawable.album11
-        };
-
-        Merchant a = new Merchant("FlipKart", 13, covers[0]);
-        albumList.add(a);
-
-        a = new Merchant("Amazon", 8, covers[1]);
-        albumList.add(a);
-
-        a = new Merchant("ebay 5", 11, covers[2]);
-        albumList.add(a);
-
-        a = new Merchant("Snapdeal", 12, covers[3]);
-        albumList.add(a);
-
-        a = new Merchant("BookMyShow", 14, covers[4]);
-        albumList.add(a);
-
-        a = new Merchant("OLA", 1, covers[5]);
-        albumList.add(a);
-
-        a = new Merchant("Loud", 11, covers[6]);
-        albumList.add(a);
-
-        a = new Merchant("Legend", 14, covers[7]);
-        albumList.add(a);
-
-        a = new Merchant("Hello", 11, covers[8]);
-        albumList.add(a);
-
-        a = new Merchant("Greatest Hits", 17, covers[9]);
-        albumList.add(a);
-
-        adapter.notifyDataSetChanged();
-    }
-
-    /**
-     * RecyclerView item decoration - give equal margin around grid item
-     */
-    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
-
-        private int spanCount;
-        private int spacing;
-        private boolean includeEdge;
-
-        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
-            this.spanCount = spanCount;
-            this.spacing = spacing;
-            this.includeEdge = includeEdge;
-        }
-
-        @Override
-        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            int position = parent.getChildAdapterPosition(view); // item position
-            int column = position % spanCount; // item column
-
-            if (includeEdge) {
-                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
-                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
-
-                if (position < spanCount) { // top edge
-                    outRect.top = spacing;
-                }
-                outRect.bottom = spacing; // item bottom
-            } else {
-                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
-                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
-                if (position >= spanCount) {
-                    outRect.top = spacing; // item top
-                }
-            }
-        }
-    }
-
-    /**
-     * Converting dp to pixel
-     */
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
-    }
 
 
     boolean doubleBackToExitPressedOnce = false;
