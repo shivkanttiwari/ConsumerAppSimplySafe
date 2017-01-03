342345d25c0e6136dbb69a1386040cba734ae48d
Shivkant Tiwari <shivkanttiwari@Shivkants-MacBook-Air.local>
2017-01-03 09:51:07 +0530

    after implementng merchant list.

--- app/src/main/java/com/example/tiuadmin/simplysafeconusmerapp/Merchant/MerchantActivity.java
+++ app/src/main/java/com/example/tiuadmin/simplysafeconusmerapp/Merchant/MerchantActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.example.tiuadmin.simplysafeconusmerapp.CustomAdapter.MerchantViewAdapter;
 import com.example.tiuadmin.simplysafeconusmerapp.R;
@@ -14,16 +15,29 @@ import java.util.List;
 public class MerchantActivity extends AppCompatActivity {
     private List<String> list = new ArrayList<String>();
     MerchantViewAdapter mAdapter;
+    private StaggeredGridLayoutManager mStaggeredLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_merchant);
 
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        arrayList();
 
+       // mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+//        mRecyclerView.setHasFixedSize(false);
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        arrayList();
+//
+//        mAdapter = new MerchantViewAdapter(list,this);
+//        mRecyclerView.setAdapter(mAdapter);
+
+
+        mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.list);
+        mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
+
+        mRecyclerView.setHasFixedSize(true); //Data size is fixed - improves performance
         mAdapter = new MerchantViewAdapter(list,this);
         mRecyclerView.setAdapter(mAdapter);
 
